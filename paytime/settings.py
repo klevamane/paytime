930bc968ac90e2b77a2528b233dd8265d86eb433
Onengiye Onengiye <klevamane@gmail.com>
2020-11-25 00:15:07 +0100

    add database credentials

--- paytime/settings.py
+++ paytime/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'paytime.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("DB_NAME", "paytime"),
+        "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "password"),
+        "USER": os.getenv("DB_USER", "comfy")
     }
 }
 
