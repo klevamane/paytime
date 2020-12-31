c827e0bbe32baaef8a4b3d1e4fd0c47905776a93
Onengiye Onengiye <klevamane@gmail.com>
2020-12-31 01:09:34 +0100

    add loggin settings to setting.py

--- paytime/settings.py
+++ paytime/settings.py
@@ -197,8 +197,40 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "paytime/static")]
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": (
+                "%(asctime)s [%(process)d] [%(levelname)s] "
+                + "pathname=%(pathname)s lineno=%(lineno)s "
+                + "funcname=%(funcName)s %(message)s"
+            ),
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+        },
+        "simple": {"format": "%(levelname)s %(message)s"},
+    },
+    "handlers": {
+        "null": {
+            "level": "DEBUG",
+            "class": "logging.NullHandler",
+        },
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "testlogger": {
+            "handlers": ["console"],
+            "level": "INFO",
+        }
+    },
+}
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), logging=False)
 AUTH_USER_MODEL = "user.User"
 
 # check this sites for precommit setup
