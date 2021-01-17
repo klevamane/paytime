e1a5b2b69c57c4c0bab235d435d97f868e454797
Onengiye Onengiye <klevamane@gmail.com>
2021-01-17 14:57:18 +0100

    add more tests

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -2,17 +2,17 @@ from __future__ import absolute_import
 
 import json
 
-from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import (
     check_password,
     is_password_usable,
     make_password,
 )
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.urls import reverse
 
+from dashboard.models import MessageCenter
 from paytime import settings
-from user.models import User
 
 
 class MessageCenterViewTest(TestCase):
@@ -62,3 +62,15 @@ class MessageCenterViewTest(TestCase):
         response = self.client.get(reverse("message_inbox_view_url"))
         self.assertEqual(response.status_code, 200)
         self.assertTrue("inbox" in response.content.decode("utf-8"))
+
+    def test_send_admin_msg(self):
+        total_messages = MessageCenter.objects.all().count()
+        response = self.client.post(
+            reverse("message_new_view_url"),
+            {"message": "This is a message to the admin", "subject": "Subject"},
+        )
+        self.assertEqual(MessageCenter.objects.all().count(), total_messages + 1)
+        # assert that the message was sent by the logged in user
+        last_msg = MessageCenter.objects.all().order_by("id").last()
+        self.assertEqual(last_msg.sender, response.wsgi_request.user)
+        self.assertEqual(last_msg.subject, "Subject")
